36b06ceda17a8797c66fec06bfdbb9362438fe71
wincenty klapek <k.klapaucjusz@pm.me>
2024-05-13 16:10:24 +0200

    at the end of the day: 2024-05-13

---
+++ Nauka_01/xml/InterfaceGenerator.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace Basic_Openness
+{
+    
+
+    public class InterfaceGenerator
+    {
+
+        private XNamespace ns;
+
+        public InterfaceGenerator() {
+            ns = "http://www.siemens.com/automation/Openness/SW/Interface/v5";
+        }
+
+        public InterfaceGenerator(XNamespace ns)
+        {
+            this.ns = ns;
+        }
+        
+        // Główna metoda generująca sekcję Interface
+        public XElement GenerateInterfaceSection(InterfaceData data)
+        {
+            //XNamespace ns = "http://www.siemens.com/automation/Openness/SW/Interface/v5";
+            XElement interfaceSection = new XElement(ns + "Interface",
+                new XElement(ns + "Sections", data.Sections.Select(section => GenerateSection(section, ns)))
+            );
+            return interfaceSection;
+        }
+
+        // Metoda generująca pojedynczą sekcję
+        private XElement GenerateSection(SectionData section)
+        {
+            XElement sectionElement = new XElement(ns + "Section",
+                new XAttribute("Name", section.Name),
+                section.Members.Select(member => GenerateMember(member, ns))
+            );
+            return sectionElement;
+        }
+
+        // Metoda generująca elementy Member
+        private XElement GenerateMember(MemberData member)
+        {
+            XElement memberElement = new XElement(ns + "Member",
+                new XAttribute("Name", member.Name),
+                new XAttribute("Datatype", member.DataType),
+                member.Comment != null ? new XElement(ns + "Comment", GenerateMultiLanguageText(member.Comment, ns)) : null,
+                member.Attributes != null ? new XElement(ns + "AttributeList", member.Attributes.Select(attr => GenerateBooleanAttribute(attr, ns))) : null,
+                member.StartValue != null ? new XElement(ns + "StartValue", member.StartValue) : null
+            );
+            return memberElement;
+        }
+
+        // Metoda generująca komentarze wielojęzyczne
+        private XElement GenerateMultiLanguageText(MultiLanguageText text)
+        {
+            return new XElement(ns + "MultiLanguageText",
+                new XAttribute("Lang", text.Language),
+                text.Value
+            );
+        }
+
+        // Metoda generująca atrybuty Boolean
+        private XElement GenerateBooleanAttribute(BooleanAttribute attribute)
+        {
+            return new XElement(ns + "BooleanAttribute",
+                new XAttribute("Name", attribute.Name),
+                new XAttribute("SystemDefined", attribute.SystemDefined),
+                attribute.Value
+            );
+        }
+        private XElement CreateTemp(string name, string datatype, string comment = null, string language = "en-US")
+        {
+            MemberData memberData = new MemberData();
+            memberData.Name = name;
+            memberData.DataType = datatype;
+            memberData.Comment.Value = comment;
+            memberData.Comment.Language = language;
+            memberData.Attributes = null;
+            memberData.StartValue = null;
+        return GenerateMember(memberData);
+        }
+        private XElement CreateConstant(string name, string datatype, string startValue = null, string comment = null, string language = "en-US")
+        {
+            MemberData memberData = new MemberData();
+            memberData.Name = name;
+            memberData.DataType = datatype;
+            memberData.Comment.Value = comment;
+            memberData.Comment.Language = language;
+            memberData.Attributes = null;
+            memberData.StartValue = startValue;
+            return GenerateMember(memberData);
+        }
+
+        private XElement CreateInput(); //dokończyć
+
+
+    }
+
+    // Klasy danych
+    public class InterfaceData
+    {
+        public List<SectionData> Sections { get; set; }
+    }
+
+    public class SectionData
+    {
+        public string Name { get; set; }
+        public List<MemberData> Members { get; set; }
+    }
+
+    public class MemberData
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public MultiLanguageText Comment { get; set; }
+        public List<BooleanAttribute> Attributes { get; set; }
+        public string StartValue { get; set; }
+    }
+
+    public class MultiLanguageText
+    {
+        public string Language { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class BooleanAttribute
+    {
+        public string Name { get; set; }
+        public bool SystemDefined { get; set; }
+        public string Value { get; set; }
+    }
+
+}
---
+++ Nauka_01/xml/InterfaceNodes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Openness
+{
+    internal class InterfaceNodes
+    {
+        public const string Ns = "http://www.siemens.com/automation/Openness/SW/Interface/v5";
+        public const string AttributeList = "AttributeList";
+        public const string Interface = "Interface";
+        public const string Sections = "Sections";
+        public const string Section = "Section";
+        public const string Member = "Member";
+        public const string Comment = "Comment";
+        public const string MultiLanguageText = "MultiLanguageText";
+        public const string BooleanAttribute = "BooleanAttribute";
+        public const string StartValue = "StartValue";
+        public const string MemoryLayout = "MemoryLayout";
+        public const string MemoryReserve = "MemoryReserve";
+        public const string Name = "Name";
+        public const string Number = "Number";
+        public const string ProgrammingLanguage = "ProgrammingLanguage";
+        public const string SetENOAutomatically = "SetENOAutomatically";
+    }
+}
