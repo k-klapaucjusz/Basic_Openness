25e18e7f89c0b9b85d29cd92b48381d76ba6e01d
wincenty klapek <k.klapaucjusz@pm.me>
2024-05-17 22:25:58 +0200

    at the end of the day: 2024-05-17

--- Nauka_01/xml/InterfaceNodes.cs
+++ Nauka_01/xml/InterfaceNodes.cs
@@ -25,14 +25,16 @@ namespace Basic_Openness
         public const string ProgrammingLanguage = "ProgrammingLanguage";
         public const string SetENOAutomatically = "SetENOAutomatically";
     }
-    public static class MemberType
+    public static class OperandType
     {
         public const string Input = "Input";
         public const string Output = "Output";
         public const string InOut = "InOut";
         public const string Static = "Static";
         public const string Temp = "Temp";
-        public const string Constans = "Constans";
+        public const string Constant = "Constant";
+        public const string GlobalVariable = "GlobalVariable";
+        public const string LiteralConstant = "LiteralConstant";
     }
 
     public static class Remanence
@@ -40,6 +42,58 @@ namespace Basic_Openness
         public const string None = null;
         public const string Retain = "Retain";
         public const string SetInIDB = "SetInIDB";
+
+    }
+    public static class Token
+    {
+        // Syntax elements
+        public const string Assign = @":=";
+        public const string Out = @"=&gt";
+        public const string BracketL = @"(";
+        public const string BracketR = @")";
+        public const string EndOfExpr = @";";
+        public const string Dot = @".";
+
+        // Arythmetical Operators
+        public const string Add = @"+";
+        public const string Sub = @"-";
+        public const string Mul = @"*";
+        public const string Exp = @"**";
+        public const string Div = @"/";
+        public const string Mod = @"MOD";
+
+        // Comparison Operators
+        public const string Equal = @"=";
+        public const string NotEqual = @"<>";
+        public const string GreaterThan = @">";
+        public const string LessThan = @"<";
+        public const string GreaterThanOrEqual = @">=";
+        public const string LessThanOrEqual = @"<=";
+
+        // Logical Operators
+        public const string And = @"AND";
+        public const string Or = @"OR";
+        public const string Xor = @"XOR";
+        public const string Not = @"NOT";
+
+        // Other Operators
+        //public const string Concat = @"||";  // String concatenation in SQL
+        //public const string In = @"IN";
+        //public const string NotIn = @"NOT IN";
+        //public const string Like = @"LIKE";
+        //public const string NotLike = @"NOT LIKE";
+        //public const string Is = @"IS";
+        //public const string IsNot = @"IS NOT";
+        //public const string Between = @"BETWEEN";
+        //public const string NotBetween = @"NOT BETWEEN";
+        //public const string Exists = @"EXISTS";
+        //public const string NotExists = @"NOT EXISTS";
+
+        //// Bitwise Operators
+        //public const string BitwiseAnd = @"&";
+        //public const string BitwiseOr = @"|";
+        //public const string BitwiseXor = @"^";
+        //public const string BitwiseNot = @"~";
 
     }
 }
---
+++ Nauka_01/xml/SclWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Openness
+{
+    internal class SclWrapper
+    {
+        public class SclAssignment
+        {
+
+            public SclAssignment(Operand leftSide, List<ISclSyntax> rightSide) { }
+
+        }
+        public interface ISclSyntax { }
+
+        public class LiteralCost : ISclSyntax
+        {
+            public string Value { get; set; }
+            public const string DataType = "LiteralConstant";
+        }
+
+        public class Operand : ISclSyntax
+        {
+            // mniej więcej taka zawartość, punkt wyjścia
+            public string MemberType { get; set; }
+            public string Name { get; set; }
+            public string DataType { get; set; }
+            public MultiLanguageText Comment { get; set; }
+            public List<BooleanAttribute> Attributes { get; set; }
+            public string StartValue { get; set; }
+            public string IsRetain { get; set; }
+            public string IsSetPoint { get; set; }
+        }
+
+
+        public class SclToken : ISclSyntax
+        {
+            public string Value { get; set; }
+        }
+        //public class SclOperand : ISclSyntax
+        //{
+        //    public string Value { get; set; }
+        //}
+
+    }
+}
